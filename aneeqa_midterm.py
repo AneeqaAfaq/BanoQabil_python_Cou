631a062e713ccc94021fadd4d154a218c7f398b7
AneeqaAfaq <156565310+AneeqaAfaq@users.noreply.github.com>
2024-01-21 07:17:08 +0000

    Add files via upload

---
+++ aneeqa_midterm.py
@@ -0,0 +1,71 @@
+#Aneeqa Afaq
+# Q1: Write a python program to do arithmetical operations addition and division.
+
+num1 = int(input("Enter the number : "))
+num2 = int(input("Enter the number : "))
+sum = num1 + num2
+print("Addition: ",num1," + ",num2," = ",sum)
+
+num1 = int(input("Enter the number : "))
+num2 = int(input("Enter the number : "))
+sum = num1 / num2
+print("Division: ",num1," / ",num2," = ",sum)
+
+num1 = int(input("Enter the number : "))
+num2 = int(input("Enter the number : "))
+sum = num1 ** num2
+print("Power: ",num1," ^ ",num2," = ",sum)
+
+num1 = int(input("Enter the number: "))
+num2 = int(input("Enter the number: "))
+sum = num1 // num2
+print("Floor division: ",num1," // ",num2," = ", sum)
+
+
+
+# Q2: Write a python program to find the area of triangle.
+length = 10
+height = 15
+sum = 1/2*length*height
+print("The area of the triangle is: ",sum)
+
+
+# Q3: Write a python to convert Celcius into farenheit.
+
+celcius = 37
+f = (9/5*celcius)+32
+print("37 degrees is equal to : ",f)
+
+
+# Q4: Write a python that return all data types that we discussed in class.
+
+num1 = int(input("Enter the number : "))
+print("The data types of",num1," is: ",type(num1))
+
+num1 = 10.5
+print("The data type of ",num1," is equal to : ",type(num1))
+
+num1 = str(input("Enter the name: " ))
+print("The data typeof ",num1," is equal to : " ,type(num1))
+
+num1 = True
+print("The data type of ",num1," is " ,type(num1))
+
+lst = [1,2,3,4,5]
+print("The data type of list is : ",type(lst))
+
+tpl = (1,2,3,4,5)
+print("The data type of tuple is : ",type(tpl))
+
+sett = {1,2,3,4,5}
+print("The data type of set is: ",type(sett))
+
+
+
+
+
+
+
+
+
+
